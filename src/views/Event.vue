22d01443d7af65e53df534c407fae6f0284be50f
alireza-shahbaziyan <alishahbazi0831@gmail.com>
2022-08-28 02:46:39 +0430

    db

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,31 +1,37 @@
 <template >
-<div class="Home">
-  <EventCard class="event" v-for="event in events" :key="event.id" :event="event"/>  
-</div>
+  <div class="Home">
+    <EventCard
+      class="event"
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    />
+  </div>
 </template>
 
 <script>
-  import EventCard from '@/components/EventCard.vue'
-  export default{
-    name:'Event',
-    components:{
-      EventCard
-    },
-    data(){
-        return {
-           events: [
-                { id: "5001", type: "None" },
-                { id: "5002", type: "Glazed" },
-                { id: "5005", type: "Sugar" },
-                { id: "5007", type: "Powdered Sugar" },
-            ]
-        }
-    }
-}
+import EventCard from "@/components/EventCard.vue";
+import axios from "axios";
+export default {
+  name: "Event",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    };
+  },
+  created() {
+    axios.get(
+      "https://my-json-server.typicode.com/Alireza-shahbaziyan/tutorial-vue/db"
+    ).then(res =>this.events = res.data).catch(erorr=>console.log(erorr))
+  },
+};
 </script>
 <style scoped>
-.Home{
-/* width: 100%; */
+.Home {
+  /* width: 100%; */
   display: flex;
   flex-direction: column;
   align-items: center;
